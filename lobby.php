c511df123cbed345353d14db6c01b0dc09fb81c6
Kasjan <kasjan.bojko.gdansk@gmail.com>
2023-03-24 12:22:17 +0100

    update

--- lobby.php
+++ lobby.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['zalogowany']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Szaszki.com</title>
-    <link rel="stylesheet" href="gra.css">
+    <link rel="stylesheet" href="lobby.css">
 </head>
 <body>
 <header class="header">
